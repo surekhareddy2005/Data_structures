215e837a2534a5fee6409282f6806103ec123899
surekhareddy2005 <147473990+surekhareddy2005@users.noreply.github.com>
2024-07-19 13:23:37 +0530

    Add files via upload

---
+++ array_recursion_product.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int print_array(int *a,int i,int size)
+{
+	if(i==size)
+	return 1;
+	else
+	return a[i]*print_array(a,i+1,size);
+}
+int main()
+{
+	int n,i;
+	printf("enter the no of elements\n");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter the elements of array\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("%d",print_array(arr,0,n));
+	
+}
